bdfe0cb6a99f42b813cd30352e010e5ec5a411af
trushnarabadia <rabadiatrushna@gmail.com>
2022-08-16 15:59:34 +0100

    unique product slug added to product pages

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,3 +1,13 @@
-function ProductPage() {}
+import { useParams } from 'react-router-dom';
+
+function ProductPage() {
+  const params = useParams();
+  const { slug } = params;
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
+}
 
 export default ProductPage;
