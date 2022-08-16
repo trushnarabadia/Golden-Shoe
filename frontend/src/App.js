2249c1cf032e809aa6937902a36d77926853595a
trushnarabadia <rabadiatrushna@gmail.com>
2022-08-16 15:20:54 +0100

    shifted product list to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,23 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
-          <h1>Products List</h1>
-          <div className="products">
-            {productData.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className="prodinfo">
-                  <p>{product.name}</p>
-                  <p>
-                    <strong>£{product.price}</strong>
-                  </p>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
-            ))}
-          </div>
+          
         </main>
       </div>
     </BrowserRouter>
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,5 +1,27 @@
+import productData from '../productData';
+
 function Home() {
-    return <div>Home</div>
+  return (
+    <div>
+      <h1>Products List</h1>
+      <div className="products">
+        {productData.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </a>
+            <div className="prodinfo">
+              <p>{product.name}</p>
+              <p>
+                <strong>£{product.price}</strong>
+              </p>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
