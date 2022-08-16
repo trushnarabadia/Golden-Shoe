7921f75ada91a261c3d363a8c24a782a837ca239
trushnarabadia <rabadiatrushna@gmail.com>
2022-08-16 15:14:17 +0100

    added routing to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,38 @@
 import productData from './productData';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Golden Shoe</a>
-      </header>
-      <main>
-        <h1>Products List</h1>
-        <div className="products">
-          {productData.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="prodinfo">
-                <p>{product.name}</p>
-                <p><strong>£{product.price}</strong></p>
-                <button>Add to Cart</button>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">Golden Shoe</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <h1>Products List</h1>
+          <div className="products">
+            {productData.products.map((product) => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="prodinfo">
+                  <p>{product.name}</p>
+                  <p>
+                    <strong>£{product.price}</strong>
+                  </p>
+                  <button>Add to Cart</button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
