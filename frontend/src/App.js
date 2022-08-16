c6290472308d918f74b0a4bd2d69464f70912649
trushnarabadia <rabadiatrushna@gmail.com>
2022-08-16 14:15:56 +0100

    layout amended on product list page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,17 @@ function App() {
       </header>
       <main>
         <h1>Products List</h1>
+        <div className='products'>
         {productData.products.map((product) => (
-          <div>
+          <div className='product' key={product.slug}>
             <img src={product.image} alt={product.name}/>
+            <div className='prodinfo'>
             <p>{product.name}</p>
             <p>{product.price}</p>
+            </div>
           </div>
         ))}
+        </div>
       </main>
     </div>
   )
